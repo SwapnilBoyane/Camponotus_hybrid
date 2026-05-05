0a6f56342b108984316e9159c61e55a205c95949
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2026-05-05 18:41:58 -0500

    Update 01_filter_PP_vcf.sh

--- 23_fixed_diff/01_filter_PP_vcf.sh
+++ 23_fixed_diff/01_filter_PP_vcf.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --chdir=./
-#SBATCH --job-name=fil_Fst
+#SBATCH --job-name=fil_AF
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --partition=nocona
+#SBATCH --ntasks=2
+#SBATCH --partition quanah
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=16G
 #SBATCH --array=1-31
 
 export PATH=~/bcftools/usr/local/bin:$PATH
@@ -18,11 +18,10 @@ workdir=/lustre/scratch/sboyane/camphybrid
 region_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/scaffolds.txt | tail -n1 )
 
 #Filter for Fst of pure individuals 
-
-vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep keeplist_pure_parental.txt --max-missing 0.8  --min-alleles 2 --max-alleles 2 --mac 10 --max-maf 0.49 \
---remove-indels --recode --recode-INFO-all --out ${workdir}/23_fixed_diff/${region_array}_mac10_Fst_PP_ind
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep keeplist_parental_diagno_final_143.txt --max-missing 0.8  --min-alleles 2 --max-alleles 2 --mac 10 \
+--remove-indels --recode --recode-INFO-all --out ${workdir}/12_digno_snps/${region_array}_mac10_AF_PP_ind_final_SNWHHN
 
 #Filter for Fst of pure and admix individuals 
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep keeplist_194.txt  --max-missing 0.8  --min-alleles 2 --max-alleles 2 --mac 2  \
+--remove-indels --recode --recode-INFO-all --out ${workdir}/12_digno_snps/${region_array}_mac2_AF_all_ind_final
 
-vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep all_PP_admix.txt --max-missing 0.8  --min-alleles 2 --max-alleles 2 --mac 10 --max-maf 0.49 \
---remove-indels --recode --recode-INFO-all --out ${workdir}/23_fixed_diff/${region_array}_mac10_Fst_all_ind
